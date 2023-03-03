9ad8859a34eda553b089077cb4f09220b610d628
Nishant Giri <106369419+raptor4444@users.noreply.github.com>
2023-03-03 17:13:22 +0530

    Basics Updated

---
+++ Basics/1. XOR.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// UNIQUE NUMBER IN ARRAY:
+
+
+void findUnique(int arr[], int size){
+
+    int ans = 0;
+
+    for(int i=0; i<size; i++){
+        ans = ans^arr[i];
+    }
+    cout<<"unique element in array is: "<<ans<<endl;
+}
+
+
+void findDuplicate(int arr[], int size){
+    int j = 1;
+    for(int i=0; i<size; i++){
+        if(arr[i]==arr[j]){
+            cout<<"duplicate element in array is: "<<arr[i]<<endl;
+            j++;
+        }
+        else{
+            j++;
+        }
+    }
+}
+
+
+
+
+int main(){
+
+
+
+
+    //SWAP OF TWO NUMBERS:
+
+    int a=4, b=5;
+
+    a = a^b;                                    // the value of a has changed to a^b.// the value of a has changed to a^b.
+
+    
+        
+    b = b^a;                                    /*  
+                                                after execution of statement 1, the value of a is a^b. 
+                                                execution takes place like: b = b^(a^b).
+                                                After execution of this statement the value of b becomes a.
+                                                */ 
+    
+         
+
+    a = a^b;                                    /*
+                                                After execution of statement 2, the value of b becomes a.
+                                                execution took place like: a = (a^b)^a.  where a = a^b; b = a;
+                                                After execution of this statement the value of a becomes b. 
+                                                */ 
+
+    cout<<a<<" "<<b<<endl;                      //now a = 5; b = 4;
+
+
+
+
+    //FINDING UNIQUE NUMBER IN ARRAY:
+
+
+    int arr[5] = {1,2,3,4,8};
+
+    findUnique(arr, 5);
+    findDuplicate(arr,5);
+    
+    return 0;
+
+}
